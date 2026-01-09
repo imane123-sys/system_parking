30f5c53ce3f1bea6b670888fc8947fccdb9248e1
Imane Ramadane <ramadaneimane@gmail.com>
2026-01-09 17:03:51 +0100

    fetch api

--- script.js
+++ script.js
@@ -9,29 +9,31 @@ document.querySelector("form").addEventListener("submit", function (e) {
     return;
   }
 
-  const exists = data.vehicles.some(v => v.plateNumber === plate && v.exitTime === null);
-  if (exists) {
-    alert(" Ce véhicule est déjà dans le parking !");
-    return;
-  }
-
-  const freeSlot = data.parkingSlots.find(slot => !slot.occupied);
-  if (!freeSlot) {
-    alert("Parking complet !");
-    return;
-  }
-
   const newVehicle = {
-    id: data.vehicles.length + 1,
     plateNumber: plate,
     type: type,
     entryTime: new Date().toISOString(),
     exitTime: null,
-    slotNumber: freeSlot.number
+    slotNumber: 1,
   };
 
-  data.vehicles.push(newVehicle);
-  freeSlot.occupied = true;
+  fetch("http://localhost:3000/vehicles", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newVehicle),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(" Véhicule ajouté :", data);
+      alert(`Véhicule ${data.plateNumber} ajouté avec succès !`);
 
-  alert(` Véhicule ajouté à la place ${freeSlot.number}`);
+      document.getElementById("immatriculation").value = "";
+      document.querySelector("select").value = "";
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'ajout :", error);
+      alert(" Une erreur est survenue !");
+    });
 });
